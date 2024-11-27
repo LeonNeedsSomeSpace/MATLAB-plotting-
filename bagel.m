09cefd18d7d96b8b5ac66eec15e50c27c5384071
Leon Zhou <leon.zhou.ffm@gmail.com>
2024-11-27 15:01:03 +0100

    bagel.m

--- bagel.m
+++ bagel.m
@@ -27,8 +27,7 @@ z = b * sin(2*Beta) + 0.001 * sin(5*Alpha); % 决定垂直位置，带有波动
 figure;
 % 创建 3D 曲面图，不显示边缘 
 surf(x, y, z, 'EdgeColor', 'none'); 
-% 所有坐标轴长度相等 
-axis equal;
+axis equal;% 所有坐标轴长度相等 
 % 创作题目
 title('Bagel');
 xlabel('Bagel from x angle');
@@ -36,4 +35,3 @@ ylabel('Bagel from y angle');
 zlabel('Bagel height');
 % 显示网格
 grid on;
-
