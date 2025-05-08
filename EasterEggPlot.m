3f91d9e65afb4fc655799fcd72eeb74693087e4f
Leon Zhou <leon.zhou.ffm@gmail.com>
2025-05-08 20:00:22 +0200

    EasterEggPlot.m

--- EasterEggPlot.m
+++ EasterEggPlot.m
@@ -3,7 +3,7 @@
 theta = linspace(0, 2*pi, 100); 
 phi = linspace(-pi/2, pi/2, 500); 
 
-[Theta, Phi] = meshgrid(theta, phi); 
+[Theta, Phi] = meshgrid(theta, phi);  % 创建角度 theta 和 phi 的网格，用于后续的函数计算或绘图
 
 a = 40; 
 b = 48; 
